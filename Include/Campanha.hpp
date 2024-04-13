554501bbe13b2f3fd61c3e2430595c130f64f0a9
primonom <pabloreisrebonato@gmail.com>
2024-04-13 19:04:44 -0300

    atualizaçao4

--- Include/Campanha.hpp
+++ Include/Campanha.hpp
@@ -3,21 +3,12 @@
 
 #include <iostream>
 #include <string>
-#include <vector>
 
 class Campanha {
-private:
-    std::vector<std::string> opcoes = {
-        "Caverna do Dragao",
-        "Monte Ararath",
-        "Eclipse",
-        "Em busca de Atlantida",
-        "Perdidos no Espaco"
-    };
-
 public:
     void exibirOpcoes();
     std::string escolherCampanha();
 };
 
 #endif
+
--- src/Campanha.cpp
+++ src/Campanha.cpp
@@ -2,9 +2,11 @@
 
 void Campanha::exibirOpcoes() {
     std::cout << "Escolha uma campanha:" << std::endl;
-    for (int i = 0; i < opcoes.size(); ++i) {
-        std::cout << i + 1 << ". " << opcoes[i] << std::endl;
-    }
+    std::cout << "1. Caverna do Dragao" << std::endl;
+    std::cout << "2. Monte Ararath" << std::endl;
+    std::cout << "3. Eclipse" << std::endl;
+    std::cout << "4. Em busca de Atlantida" << std::endl;
+    std::cout << "5. Perdidos no Espaco" << std::endl;
 }
 
 std::string Campanha::escolherCampanha() {
@@ -12,10 +14,20 @@ std::string Campanha::escolherCampanha() {
     exibirOpcoes();
     std::cin >> escolha;
 
-    if (escolha < 1 || escolha > opcoes.size()) {
-        std::cerr << "Opcao invalida!" << std::endl;
-        return "";
+    switch (escolha) {
+        case 1:
+            return "Caverna do Dragao";
+        case 2:
+            return "Monte Ararath";
+        case 3:
+            return "Eclipse";
+        case 4:
+            return "Em busca de Atlantida";
+        case 5:
+            return "Perdidos no Espaco";
+        default:
+            std::cerr << "Opcao invalida!" << std::endl;
+            return "";
     }
-
-    return opcoes[escolha - 1];
 }
+
