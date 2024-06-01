a7f229849a966942b13f75597c5561cd3cb16ef2
Gabriel Mora Bissoli <gabrielmbissoli@gmail.com>
2024-06-01 16:43:47 -0300

    atualização na classe sorte

--- Include/Sorte.hpp
+++ Include/Sorte.hpp
@@ -1,13 +1,25 @@
 #ifndef SORTE_HPP
 #define SORTE_HPP
 
+#include <iostream>
 #include <utility>
 
 class Sorte {
+private:
+    int resultado;
+    double porcentagem;
+
 public:
-    Sorte();
-    std::pair<int, double> lancarDado();
+    Sorte(); // Construtor
+    ~Sorte(); // Destrutor
+    void lancarDado();
+
+    // Getters
+    int getResultado() const;
+    double getPorcentagem() const;
+
+    // Sobrecarga do operador <<
+    friend std::ostream& operator<<(std::ostream& os, const Sorte& sorte);
 };
 
 #endif
-
--- src/Sorte.cpp
+++ src/Sorte.cpp
@@ -2,17 +2,32 @@
 #include <cstdlib>
 #include <ctime>
 
-Sorte::Sorte() {
+Sorte::Sorte() : resultado(0), porcentagem(0.0) {
     // Inicializa a semente do gerador de números aleatórios
     std::srand(std::time(0));
 }
 
-double Sorte::lancarDado() {
+Sorte::~Sorte() {
+    std::cout << "O objeto Sorte foi destruído." << std::endl;
+}
+
+void Sorte::lancarDado() {
     // Gera um número aleatório entre 1 e 6
-    int resultado = std::rand() % 6 + 1;
+    resultado = std::rand() % 6 + 1;
 
     // Calcula a porcentagem de sorte
-    double sorte = (resultado - 1) * 100.0 / 5.0;
+    porcentagem = (resultado - 1) * 100.0 / 5.0;
+}
 
-    return sorte;
+int Sorte::getResultado() const {
+    return resultado;
+}
+
+double Sorte::getPorcentagem() const {
+    return porcentagem;
+}
+
+std::ostream& operator<<(std::ostream& os, const Sorte& sorte) {
+    os << "Sorte: Resultado do dado = " << sorte.getResultado() << ", Porcentagem de sorte = " << sorte.getPorcentagem() << "%";
+    return os;
 }
