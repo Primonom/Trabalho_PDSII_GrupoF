322fc40dc1023166e31ae68b6ee2bfe9216f3b45
Gabriel Mora Bissoli <gabrielmbissoli@gmail.com>
2024-06-01 16:23:24 -0300

    economia atualizada

--- Include/modulo2/Economia.hpp
+++ Include/modulo2/Economia.hpp
@@ -1,14 +1,19 @@
 #ifndef ECONOMIA_HPP
 #define ECONOMIA_HPP
 
+#include <iostream>
+
 class Economia {
 public:
     Economia(); // Construtor
     ~Economia(); // Destrutor
 
     void ganharOuro(int quantidade);
-    void gastarOuro(int quantidade);
+    bool gastarOuro(int quantidade); // Retorna true se a operação foi bem-sucedida, false caso contrário
     int getOuro() const;
+
+    // Sobrecarga do operador <<
+    friend std::ostream& operator<<(std::ostream& os, const Economia& economia);
 
 private:
     int ouro;
--- src/modulo2/Economia.cpp
+++ src/modulo2/Economia.cpp
@@ -1,26 +1,34 @@
-#include "economia.hpp"
+#include "Economia.hpp"
 
 Economia::Economia() {
     ouro = 0; // Inicializa o ouro do jogador como 0 ao iniciar o jogo
 }
 
 Economia::~Economia() {
-    // Implementação do destrutor, se necessário
+    // Implementação do destrutor
+    std::cout << "O objeto Economia foi destruído." << std::endl;
 }
 
 void Economia::ganharOuro(int quantidade) {
     ouro += quantidade; // Adiciona a quantidade de ouro ganha ao total
 }
 
-void Economia::gastarOuro(int quantidade) {
+bool Economia::gastarOuro(int quantidade) {
     if (ouro >= quantidade) {
         ouro -= quantidade; // Subtrai a quantidade de ouro gasta do total, se houver ouro suficiente
+        return true;
     } else {
-        // Aqui você pode lidar com a situação de o jogador não ter ouro suficiente para gastar
-        // Por exemplo, exibir uma mensagem de erro ou retornar um código de erro
+        std::cout << "Você não tem ouro suficiente para gastar." << std::endl;
+        return false;
     }
 }
 
 int Economia::getOuro() const {
     return ouro; // Retorna a quantidade atual de ouro do jogador
+}
+
+// Implementação da sobrecarga do operador <<
+std::ostream& operator<<(std::ostream& os, const Economia& economia) {
+    os << "Ouro: " << economia.getOuro();
+    return os;
 }
